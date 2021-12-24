cb1460ba09a3a2614ce8a562222b6d15b1547234
st-a-novoseltcev <st.a.novoseltcev@gmail.com>
2021-12-24 03:53:07 +0300

    init category business-logic interfaces

---
+++ server/services/category/__init__.py
@@ -0,0 +1,13 @@
+from .interactor import CategoryInteractor, CategoryInputData
+from .repository import CategoryRepository
+from .entity import Category
+from .service import CategoryService
+
+__all__ = [
+    'CategoryInteractor',
+    'CategoryInputData',
+    'CategoryRepository',
+    'Category',
+    'CategoryService',
+    
+]
