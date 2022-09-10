a81def92b4ae45b3be31087f200b50f652d46e5b
novoseltcev <novoseltcev.stanislav@gmail.com>
2022-09-10 12:25:18 +0300

    define core and core config

---
+++ app/__init__.py
@@ -0,0 +1,38 @@
+import os
+
+from flask import Flask
+from flask.cli import load_dotenv
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+
+def create_app() -> Flask:
+    app = Flask(__name__, instance_relative_config=True)
+    app.url_map.strict_slashes = False
+    CORS(app, expose_headers=["Content-Disposition"])
+
+    load_dotenv('.env')
+    from app.config import config
+    app.config.from_object(config)
+
+    for key in ['TMP_FILE_PATH', 'PERMANENT_FILE_PATH']:
+        os.makedirs(app.config[key], exist_ok=True)
+
+    app.app_context().push()
+
+    from app.db import db
+    Migrate(app, db)
+    db.init_app(app)
+
+    from app.models import (
+        User, Category, Task, File
+    )
+
+    from .routes import register_routes
+    register_routes(app)
+
+    return app
+
+
+if __name__ == '__main__':
+    create_app().run()
--- app/config.py
+++ app/config.py
@@ -1,39 +1,38 @@
-from os import environ
 from datetime import timedelta
-
-from dotenv import load_dotenv
-
-
-load_dotenv('.env')
+from distutils.util import strtobool
+from os import environ
 
 
-class Config:
-    USE_PERMANENT_SESSION = True
-    PERMANENT_SESSION_LIFETIME = timedelta(days=5)
+class __DBConfig:
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = strtobool(environ.get('SQLALCHEMY_TRACK_MODIFICATIONS'))
 
-    SECRET_KEY = environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URL = environ.get('DATABASE_URL')
 
+class __AWSConfig:
+    AWS_ACCESS_KEY = environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_KEY = environ.get('AWS_SECRET_KEY')
+    AWS_BUCKET_NAME = environ.get('AWS_BUCKET_NAME')
+
+
+class __JWTConfig:
     JWT_SECRET_KEY = environ.get("JWT_SECRET_KEY")
     JWT_TOKEN_LOCATION = ['headers', 'cookies']
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=15)
 
-    MAIL_USERNAME = environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = environ.get('MAIL_SENDER')
 
-    MAIL_CONFIRM_TOKEN_EXPIRES = timedelta(days=1)
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-
-    AWS_ACCESS_KEY = environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_KEY = environ.get('AWS_SECRET_KEY')
-    AWS_BUCKET_NAME = environ.get('AWS_BUCKET_NAME')
-
+class __BROKERConfig:
     CELERY_BROKER_URL = environ.get('CELERY_BROKER_URL')
     BACKEND_RESULT = environ.get('CELERY_RESULT_BACKEND')
+
+
+class Config(__DBConfig, __JWTConfig, __BROKERConfig, __AWSConfig):
+    DEBUG: bool = strtobool(environ.get('DEBUG', default='False'))
+    SECRET_KEY: str = environ.get('SECRET_KEY')
+    AUTH_DISABLE: bool = strtobool(environ.get('AUTH_DISABLE', 'False'))
+
+    TMP_FILE_PATH: str = environ.get('TMP_FILE_PATH')
+    PERMANENT_FILE_PATH: str = environ.get('PERMANENT_FILE_PATH')
 
 
 config = Config()
